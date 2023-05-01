12cc50fdbcdf68a9d6fec0c064d23c10bec6fcf2
imsde <132277952+imsde@users.noreply.github.com>
2023-05-01 19:39:51 +0530

    main file changes

---
+++ mainds.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  1 19:37:43 2023
+
+@author: maliroh02
+"""
+
+print('in main code')
+
+def main():
+    l = [2,4,6,8,9]
+    for i in l:
+        print(l)
